7a30c9c74b2213f0351cf6c457faeba74679c16c
Christoph Krieger <christoph.krieger@anaqor.io>
2023-12-19 16:58:01 +0100

    init

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input v-model="name" />
+    <p>Result: {{ result }}</p>
+    <button @click="submit">Submit</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const name = ref('')
+const result = ref('')
+async function submit() {
+  const { body } = await $fetch('/api/submit', {
+    method: 'post',
+    body: { test: name.value }
+  })
+  result.value = body
+}
+</script>
