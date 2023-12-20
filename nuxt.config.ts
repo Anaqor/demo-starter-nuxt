932ba319664ceb5d88898b24ae0d0dd0b78f764f
Christoph Krieger <christoph.krieger@anaqor.io>
2023-12-20 15:48:12 +0100

    some changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["nuxt-security"],
-  security: {
-    headers: {
-      crossOriginResourcePolicy: 'cross-origin',
-      crossOriginEmbedderPolicy: false
-    },
+  //modules: ["nuxt-security"],
+  nitro: {
+    routeRules: {
+      '/': { cors: true, headers: { 'access-control-allow-methods': 'GET, POST, OPTIONS' } },
+    }
   },
+  // security: {
+  //   headers: {
+  //     crossOriginResourcePolicy: 'cross-origin',
+  //     crossOriginEmbedderPolicy: false
+  //   },
+  // },
   css: ["bootstrap/dist/css/bootstrap.min.css"],
   devtools: { enabled: true }
 })
