224e045ad4ebbdc118704ae1df4f4aa0eb754b70
Christoph Krieger <christoph.krieger@anaqor.io>
2023-12-20 14:57:00 +0100

    add nuxt security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["nuxt-security"],
+  security: {
+    headers: {
+      crossOriginResourcePolicy: 'cross-origin',
+    },
+  },
   css: ["bootstrap/dist/css/bootstrap.min.css"],
   devtools: { enabled: true }
 })
