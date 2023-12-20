7718ef439c041e85965a09a3bdda08f15aec493d
Christoph Krieger <christoph.krieger@anaqor.io>
2023-12-20 16:07:49 +0100

    remove nuxt security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  //modules: ["nuxt-security"],
   nitro: {
     routeRules: {
       '/': { cors: true, headers: { 'access-control-allow-methods': 'GET, POST, OPTIONS' } },
     }
   },
-  // security: {
-  //   headers: {
-  //     crossOriginResourcePolicy: 'cross-origin',
-  //     crossOriginEmbedderPolicy: false
-  //   },
-  // },
   css: ["bootstrap/dist/css/bootstrap.min.css"],
   devtools: { enabled: true }
 })
