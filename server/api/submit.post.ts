9434af934752afa43d54be72529b83e1baf1eaae
Christoph Krieger <christoph.krieger@anaqor.io>
2023-12-20 14:35:40 +0100

    add planqk-service-sdk

--- server/api/submit.post.ts
+++ server/api/submit.post.ts
@@ -1,4 +1,22 @@
+import {PlanqkServiceClient} from "@anaqor/planqk-service-sdk"
+
 export default defineEventHandler(async (event) => {
-    const body = await readBody(event)
-    return { body }
-  })
+  const body = await readBody(event)
+
+  const serviceEndpoint = process.env.SERVICE_ENDPOINT || ''
+  const consumerKey = process.env.CONSUMER_KEY || ''
+  const consumerSecret = process.env.CONSUMER_SECRET || ''
+
+  const client = new PlanqkServiceClient(serviceEndpoint, consumerKey, consumerSecret)
+
+  // prepare your input data and parameters
+  const data = {"n_numbers": parseInt(body.count)}
+  const params = {"n_bits": 4, "backend": "qasm_simulator"}
+
+  // start the execution
+  let job = await client.startExecution({ data, params })
+
+  // wait for the result
+  const result = await client.getResult(job.id!)
+  return result
+})
